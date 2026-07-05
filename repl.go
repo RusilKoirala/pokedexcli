14e06b787ae26e67ce54b8732e1612805487c763
Rusil Koirala <rusilkoirala23@gmail.com>
2026-07-05 16:11:54 +0545

    basic setup working, going to fix else

--- repl.go
+++ repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func StartRepl() {
@@ -13,7 +14,34 @@ func StartRepl() {
 		fmt.Printf("> ")
 		scanner.Scan()
 		text := scanner.Text()
-		fmt.Println(text)
+		cleaned := cleanInput(text)
+
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		command := cleaned[0]
+
+		switch command {
+		case "help":
+			fmt.Println("Pokedex Menu")
+			fmt.Println("1. - help")
+			fmt.Println("2. - exit")
+			fmt.Println(" ")
+		case "exit":
+			fmt.Println("Exiting...")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Invalid Command")
+		}
+
 	}
 
+}
+
+func cleanInput(str string) []string {
+	loweredString := strings.ToLower(str)
+	words := strings.Fields(loweredString)
+	return words
 }
